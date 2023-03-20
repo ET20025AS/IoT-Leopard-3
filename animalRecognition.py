64350fa5deba957f5bf909795bf0954d4bf94e96
ET20025AS <et20025@lehre.dhbw-stuttgart.de>
2023-03-20 13:15:33 +0100

    Recognition of multiple animals and list creation

--- animalRecognition.py
+++ animalRecognition.py
@@ -10,17 +10,25 @@ with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 
 # Definieren der Schwellenwerte für die Erkennung
-conf_threshold = 0.5
+conf_threshold = 0.8
 nms_threshold = 0.4
 
 # Starten der Kamera
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('Birds_12___4K_res.mp4')
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture('Birds_12___4K_res.mp4')
+
+# benennen Sie das Fenster um und setzen Sie den Modus auf cv2.WINDOW_NORMAL
+cv2.namedWindow("Animal Detection", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty("Animal Detection", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+# Definieren der zu erkennenden Klassen
+animal_classes = ["bird", "cat", "dog", "horse", "sheep", "cow", "bear", "person"]
+animal_class_ids = [classes.index(animal_class) for animal_class in animal_classes]
 
 while True:
     # Lesen des Kamerabildes
     success, img = cap.read()
-    
+
     # Erstellen eines Blob-Objekts aus dem Eingabebild
     blob = cv2.dnn.blobFromImage(img, 1/255.0, (416, 416), swapRB=True, crop=False)
 
@@ -32,40 +40,44 @@ while True:
     output_layers = [layer_names[i-1] for i in net.getUnconnectedOutLayers()]
     outputs = net.forward(output_layers)
 
-    # Erkennung von Vögeln
-    bird_class_id = 14
-    bird_boxes = []
-    bird_confidences = []
+    # Erkennung der Tiere
+    animal_boxes = []
+    animal_classes_detected = []
+    animal_confidences = []
     for output in outputs:
         for detection in output:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if class_id == bird_class_id and confidence > conf_threshold:
+            if class_id in animal_class_ids and confidence > conf_threshold:
                 center_x = int(detection[0] * img.shape[1])
                 center_y = int(detection[1] * img.shape[0])
                 width = int(detection[2] * img.shape[1])
                 height = int(detection[3] * img.shape[0])
                 left = int(center_x - width / 2)
                 top = int(center_y - height / 2)
-                bird_boxes.append([left, top, width, height])
-                bird_confidences.append(float(confidence))
+                animal_boxes.append([left, top, width, height])
+                animal_classes_detected.append(animal_classes[animal_class_ids.index(class_id)])
+                animal_confidences.append(float(confidence))
 
     # Anwenden der Nicht-Maximum-Unterdrückung
-    indices = cv2.dnn.NMSBoxes(bird_boxes, bird_confidences, conf_threshold, nms_threshold)
+    indices = cv2.dnn.NMSBoxes(animal_boxes, animal_confidences, conf_threshold, nms_threshold)
 
-    # Markierung der erkannten Vögel
-    colors = np.random.uniform(0, 255, size=(len(bird_boxes), 3))
+    # Markierung der erkannten Tiere
+    colors = np.random.uniform(0, 255, size=(len(animal_boxes), 3))
+    species_array = []
     if len(indices) > 0:
         for i in indices.flatten():
-            x, y, w, h = bird_boxes[i]
-            label = f"{classes[bird_class_id]}: {bird_confidences[i]:.2f}"
+            x, y, w, h = animal_boxes[i]
+            label = f"{animal_classes_detected[i]}: {animal_confidences[i]:.2f}"
             color = colors[i]
             cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
             cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            species_array.append({"Species": animal_classes_detected[i], "Position": [x, y, w, h]})
 
     # Anzeigen des Bildes
-    cv2.imshow("Bird Detection", img)
+    print(species_array)
+    cv2.imshow("Animal Detection", img)
     
     # Abbrechen bei Drücken der "q"-Taste
     if cv2.waitKey(1) & 0xFF == ord('q'):
